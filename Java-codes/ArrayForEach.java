0738792ea82b0587d8849894109e9e62045fda55
PAVANI-90 <pavani460910@gmail.com>
2026-02-02 14:54:08 +0530

    Added: Array Examples for Better Understanding.

---
+++ Java-codes/ArrayForEach.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ArrayForEach{
+public static void main(String[] args){
+int [] marks ={12,14,16,18,20};
+System.out.println("Array elements:");
+for (int m : marks){
+Sysytem.out.println(m);
+}
+}
+}
