0a113d3b05d497957b2563385a2a0ea0b577cd8f
PAVANI-90 <pavani460910@gmail.com>
2026-03-14 23:26:47 +0530

    Added: Programs for interfaces-abstraction.

---
+++ Abstraction/src/com/gla/abstraction/TemperatureAlert.java
@@ -0,0 +1,10 @@
+package com.gla.abstraction;
+
+import java.util.function.Predicate;
+public class TemperatureAlert {
+    public static void main(String[] args) {
+        Predicate<Double> alert = temp -> temp > 40;
+        System.out.println(alert.test(45.0));
+    }
+}
+
